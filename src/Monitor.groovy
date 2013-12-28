5aa799e643ab0b0364214b3b6425aca177cd3db1
Mike Munhall <munhall.mike@gmail.com>
2013-12-28 10:35:25 -0700

    Read temp, convert to F.

--- src/Monitor.groovy
+++ src/Monitor.groovy
@@ -1,5 +1,7 @@
 def config = new ConfigSlurper().parse(new File('/Users/michaelmunhall/Documents/workspace/ds18b20-monitor/conf/local.groovy').toURL())
 
 def data = new File(config.data.source).text
+def cTemp = data.readLines()[1].split("=")[1] as float
+def fTemp = ((cTemp / 1000) * 9 / 5) + 32
 
-println data
+println fTemp
