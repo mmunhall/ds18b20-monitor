6779c0fcc43d1cd85013e92f70261f5dcc91c989
Mike Munhall <munhall.mike@gmail.com>
2013-12-28 14:56:18 -0700

    Script for sending alerts.

--- conf/local.groovy
+++ conf/local.groovy
@@ -1,14 +1,26 @@
 data {
+    // The node (file) containing the temperature data
     source = "/Users/michaelmunhall/Documents/workspace/ds18b20-monitor/test/data/28-test"
 }
 
+polling {
+    // how often to query for the current temperature, in milliseconds. we use milliseconds because it is easier to test.
+    // to convert milliseconds to minutes, multiply by 60000.
+    interval = 5000
+}
+
 temps {
+    // the minimum allowable temperature. a reading below this will generate an alert
     min = 60
+
+    // the maximum allowable temperature. a reading above this will generate an alert
     max = 70
 }
 
 messaging {
+    // the number to send SMS alerts to
     sms = "3035149144"
-    maxFrequency = 5
+
+    // after an alert is generated this many times, stop sending alerts.
     maxConsecutiveMessages = 12
 }
--- src/Monitor.groovy
+++ src/Monitor.groovy
@@ -1,7 +1,27 @@
-def config = new ConfigSlurper().parse(new File('/Users/michaelmunhall/Documents/workspace/ds18b20-monitor/conf/local.groovy').toURL())
+def config = new ConfigSlurper().parse(URI.create("file:///Users/michaelmunhall/Documents/workspace/ds18b20-monitor/conf/local.groovy").toURL())
+def alertHistory = []
 
-def data = new File(config.data.source).text
-def cTemp = data.readLines()[1].split("=")[1] as float
-def fTemp = ((cTemp / 1000) * 9 / 5) + 32
+while (true) {
+    // read data
+    def data = new File(config.data.source).text
 
-println fTemp
+    // parse temp
+    def cTemp = (data.readLines()[1].split("=")[1]) as float
+    def fTemp = ((cTemp / 1000) * 9 / 5) + 32
+
+    // if temp is out of range, send an alert and log the event
+    if (fTemp < config.temps.min || fTemp > config.temps.max) {
+        def now = new Date()
+        println "$now Alert: $fTemp"
+        alertHistory << now
+    } else {
+        alertHistory.clear() // if all is good, then clear history
+    }
+
+    // if we've exceeded the max messages, quit. this is a spam-be-gone mechanism
+    if (alertHistory.size() > config.messaging.maxConsecutiveMessages) {
+        System.exit(0)
+    }
+
+    sleep config.polling.interval
+}
