283af13ada2e36fef14435d40166e98f49f674bb
Mike Munhall <munhall.mike@gmail.com>
2013-12-31 10:42:57 -0700

    Added pi config.

--- conf/pi.groovy
+++ conf/pi.groovy
@@ -1,25 +1,25 @@
 data {
     // The node (file) containing the temperature data
-    source = "/Users/michaelmunhall/Documents/workspace/ds18b20-monitor/test/data/28-test"
+    source = "/sys/bus/w1/devices/28-000004f20dba/w1_slave"
 }
 
 polling {
     // how often to query for the current temperature, in milliseconds. we use milliseconds because it is easier to test.
     // to convert milliseconds to minutes, multiply by 60000.
-    interval = 60000
+    interval = 5000
 }
 
 temps {
     // the minimum allowable temperature. a reading below this will generate an alert
-    min = 60
+    min = 225
 
     // the maximum allowable temperature. a reading above this will generate an alert
-    max = 70
+    max = 250
 }
 
 alerting {
     // alert type. one of "println" or "sms". println is useful for debugging.
-    type = "println"
+    type = "sms"
 
     // the details of the sms post
     sms {
